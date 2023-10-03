23244178e51b3e918ec2c4b5ab2f12ca21dd1562
isudfv <isudfv@outlook.com>
2023-10-03 20:31:02 +0800

    feat: logs

--- main.cpp
+++ main.cpp
@@ -1,24 +1,25 @@
 #include <QCoreApplication>
+#include <QFile>
 #include <QHttpServer>
-#include <QProcess>
 #include <QJsonDocument>
 #include <QJsonObject>
+#include <QProcess>
+#include <QSslKey>
 
-#include <nlohmann/json.hpp>
-#include <range/v3/all.hpp>
-
-#include <mailio/message.hpp>
-#include <mailio/smtp.hpp>
+#include "mailio/message.hpp"
+#include "mailio/smtp.hpp"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/stdout_sinks.h"
+#include "spdlog/spdlog.h"
 
 #include "DesktopNotificationManagerCompat.h"
 
-#include <iostream>
 #include "Windows.h"
-#include "DesktopNotificationManagerCompat.h"
+#include <conio.h>
 #include <functional>
+#include <iostream>
 #include <winrt/Windows.Data.Xml.Dom.h>
 #include <winrt/Windows.UI.Notifications.h>
-#include <conio.h>
 
 using namespace winrt;
 using namespace Windows::Data::Xml::Dom;
@@ -29,14 +30,13 @@ using namespace Windows::ApplicationModel;
 using namespace Windows::UI::Notifications;
 using namespace Windows::Foundation::Collections;
 
-void sendToast();
-
-QString sendEmail(QVariantMap const& map) {
+QString sendEmail(QVariantMap const& map)
+{
     using namespace mailio;
-    auto __ = [&] (QString const& name) {
+    auto __ = [&](QString const& name) {
         return map[name].toString().toStdString();
     };
-    try{
+    try {
         mailio::message msg;
         msg.header_codec(message::header_codec_t::BASE64);
         msg.from(mail_address(__("from_name"), __("from_email")));
@@ -50,9 +50,8 @@ QString sendEmail(QVariantMap const& map) {
         conn.authenticate(__("from_email"), __("Smtp-Password"), smtps::auth_method_t::START_TLS);
         conn.submit(msg);
     }
-    catch (std::exception& e)
-    {
-        qDebug() << e.what();
+    catch (std::exception& e) {
+        SPDLOG_ERROR("{}", e.what());
         return e.what();
     }
     return "Email sent!";
@@ -69,7 +68,7 @@ QString sendTextToast(QVariantMap const& map)
     // https://learn.microsoft.com/en-us/uwp/schemas/tiles/toastschema/element-image
     XmlDocument doc;
     doc.LoadXml(
-            L"<toast>\
+        L"<toast>\
     <visual>\
         <binding template=\"ToastGeneric\">\
             <text></text>\
@@ -77,9 +76,10 @@ QString sendTextToast(QVariantMap const& map)
             <image placement=\"appLogoOverride\" />\
         </binding>\
     </visual>\
-</toast>");
+</toast>"
+    );
 
-    try{
+    try {
         // Populate with text and values
         doc.DocumentElement().SetAttribute(L"launch", action_args.join('&').toStdWString());
         doc.SelectSingleNode(L"//text[1]").InnerText(map["subject"].toString().toStdWString());
@@ -95,6 +95,7 @@ QString sendTextToast(QVariantMap const& map)
         DesktopNotificationManagerCompat::CreateToastNotifier().Show(notif);
     }
     catch (hresult_error& e) {
+        SPDLOG_ERROR(L"code: {}, message: {}.", (int)e.code(), (std::wstring_view)e.message());
         return QString("code: %1, message: %2.").arg(e.code()).arg(QString::fromStdWString(e.message().data()));
     }
     return "Show Toast!";
@@ -112,67 +113,45 @@ QString sendTextToast(QVariantMap const& map)
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 
 
-int main(int argc, char *argv[]) {
-    DesktopNotificationManagerCompat::Register(L"PC.NotificationApp", L"通知中心",
-                                               L"C:\\Users\\isudfv\\Pictures\\fireworks.png");
+int main(int argc, char* argv[])
+{
+    DesktopNotificationManagerCompat::Register(L"PC.NotificationApp", L"通知中心", L"C:\\Users\\isudfv\\Pictures\\fireworks.png");
+
+    try {
+        auto logger = std::make_shared<spdlog::logger>(
+            "combined_logger",
+            spdlog::sinks_init_list{
+                std::make_shared<spdlog::sinks::stdout_sink_mt>(),
+                std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/logs.txt"),
+            }
+        );
+        spdlog::set_default_logger(logger);
+        spdlog::set_pattern("[%C-%m-%d %H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] [source %s] [function %!] [line %#] %v");
+        spdlog::set_level(spdlog::level::info);
+        spdlog::flush_on(spdlog::level::info);
+    }
+    catch (const spdlog::spdlog_ex& ex) {
+        std::cerr << "Log init failed: " << ex.what() << std::endl;
+    }
 
     DesktopNotificationManagerCompat::OnActivated([](DesktopNotificationActivatedEventArgsCompat e) {
-        qDebug() << e.Argument();
+        SPDLOG_INFO(L"On Toast Activated: {}", e.Argument());
         QString args = QString::fromStdWString(e.Argument());
         if (args.contains("action=execute")) {
             QUrlQuery query{args};
-            qDebug() << "execute : " << QProcess::startDetached(query.queryItemValue("exe_path"), query.allQueryItemValues("exe_args"));
+            SPDLOG_INFO("Execute: {}", QProcess::startDetached(query.queryItemValue("exe_path"), query.allQueryItemValues("exe_args")));
         }
-//        auto notification_id = QUrlQuery{args}.allQueryItemValues("notificationId").join(',');
-//        DesktopNotificationManagerCompat::History().Remove(notification_id.toStdWString());
-/*
-        if (e.Argument()._Starts_with(L"action=like")) {
-            sendBasicToast(L"Sent like!");
-
-            if (!_hasStarted) {
-                exit(0);
-            }
-        } else if (e.Argument()._Starts_with(L"action=reply")) {
-            std::wstring msg = e.UserInput().Lookup(L"tbReply").c_str();
-
-            sendBasicToast(L"Sent reply! Reply: " + msg);
-
-            if (!_hasStarted) {
-                exit(0);
-            }
-        } else {
-            if (!_hasStarted) {
-                if (e.Argument()._Starts_with(L"action=viewConversation")) {
-                    std::cout << "Launched from toast, opening the conversation!\n\n";
-                }
-
-//                start();
-            } else {
-                showWindow();
-
-                if (e.Argument()._Starts_with(L"action=viewConversation")) {
-                    std::cout << "\n\nOpening the conversation!\n\nEnter a number to continue: ";
-                } else {
-                    std::wcout << L"\n\nToast activated!\n - Argument: " + e.Argument() +
-                                  L"\n\nEnter a number to continue: ";
-                }
-            }
-        }
-*/
     });
 
     QCoreApplication app(argc, argv);
 
     QHttpServer http_server;
-    http_server.route("/email", QHttpServerRequest::Method::Post, [](QHttpServerRequest const &request) {
-        qDebug() << request;
-        std::optional<QString> email_content;
+    http_server.route("/email", QHttpServerRequest::Method::Post, [](QHttpServerRequest const& request) {
+        SPDLOG_INFO("[Notification] request body: {}", request.body().toStdString());
         QJsonParseError error;
         auto document = QJsonDocument::fromJson(request.body(), &error);
-        auto body =request.body();
-        qDebug() << request.body();
         if (error.error != QJsonParseError::NoError) {
-            qDebug() << error.errorString();
+            SPDLOG_ERROR("[Notification] request body: {}", error.errorString().toStdString());
             return error.errorString();
         }
         auto map = document.object().toVariantMap();
@@ -185,9 +164,8 @@ int main(int argc, char *argv[]) {
     http_server.route("/hello", QHttpServerRequest::Method::Post, []() {
         return "Hello";
     });
-    http_server.route("/toast", QHttpServerRequest::Method::Post, [](QHttpServerRequest const &request) {
-        std::optional<QString> toast_content;
-
+    http_server.route("/toast", QHttpServerRequest::Method::Post, [](QHttpServerRequest const& request) {
+        SPDLOG_INFO("[Notification] request body: {}", request.body().toStdString());
         auto document = QJsonDocument::fromJson(request.body());
         auto map = document.object().toVariantMap();
         return sendTextToast(map);
@@ -197,58 +175,4 @@ int main(int argc, char *argv[]) {
 
 
     return QCoreApplication::exec();
-}
-
-void sendToast() {
-
-    // Construct the toast template
-    XmlDocument doc;
-    doc.LoadXml(
-            L"<toast>\
-    <visual>\
-        <binding template=\"ToastGeneric\">\
-            <text></text>\
-            <text></text>\
-            <image placement=\"appLogoOverride\" hint-crop=\"circle\"/>\
-            <image/>\
-        </binding>\
-    </visual>\
-    <actions>\
-        <input\
-            id=\"tbReply\"\
-            type=\"text\"\
-            placeHolderContent=\"Type a reply\"/>\
-        <action\
-            content=\"Reply\"\
-            activationType=\"background\"/>\
-        <action\
-            content=\"Like\"\
-            activationType=\"background\"/>\
-        <action\
-            content=\"View\"\
-            activationType=\"background\"/>\
-    </actions>\
-</toast>");
-
-    // Populate with text and values
-    doc.DocumentElement().SetAttribute(L"launch", L"action=viewConversation&conversationId=9813");
-    doc.SelectSingleNode(L"//text[1]").InnerText(L"Andrew sent you a picture");
-    doc.SelectSingleNode(L"//text[2]").InnerText(L"Check this out, Happy Canyon in Utah!");
-    doc.SelectSingleNode(L"//image[1]").as<XmlElement>().SetAttribute(L"src", L"https://unsplash.it/64?image=1005");
-    doc.SelectSingleNode(L"//image[2]").as<XmlElement>().SetAttribute(L"src",
-                                                                      L"https://picsum.photos/364/202?image=883");
-    doc.SelectSingleNode(L"//action[1]").as<XmlElement>().SetAttribute(L"arguments",
-                                                                       L"action=reply&conversationId=9813");
-    doc.SelectSingleNode(L"//action[2]").as<XmlElement>().SetAttribute(L"arguments",
-                                                                       L"action=like&conversationId=9813");
-    doc.SelectSingleNode(L"//action[3]").as<XmlElement>().SetAttribute(L"arguments",
-                                                                       L"action=viewImage&imageUrl=https://picsum.photos/364/202?image=883");
-
-    // Construct the notification
-    ToastNotification notif{doc};
-
-    // And send it!
-    DesktopNotificationManagerCompat::CreateToastNotifier().Show(notif);
-
-    std::cout << "Sent!\n";
 }
